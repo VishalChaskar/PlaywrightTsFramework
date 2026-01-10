2af297733f1c9ac14b7f42d6f23a8a18fcfd45e4
vishal <chaskar.vishal7535@gmail.com>
2026-01-10 21:38:22 +0000

    changes in config and new testcases

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   reporter: [['html', { open: 'never' }]],
-  retries: 1,
+  retries: 0,
   timeout: 60000,
-  use: { headless: false }
+  use: { headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry'
+   }
 });
---
+++ tests/AutomationPractice.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('Automation Practice - Radio Button Example',() => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/',{waitUntil: 'domcontentloaded'});
+    //await page.waitForLoadState('networkidle');
+    await expect(page).toHaveURL(/.*AutomationPractice.*/);
+  });
+  test('radio section is visible and has three radios', async ({page}) => {
+    await expect(page.getByText('Radio Button Example',{ exact:true })).toBeVisible();
+    await expect(page.locator('[name="radioButton"]')).toHaveCount(3);
+  });
+  test('all radio labels are visible', async ({page}) => {
+    const labels = ['radio1','radio2','radio3'];
+    for (const label of labels){
+      await expect(page.locator(`//*[@for="${label}"]`)).toBeVisible();
+    }
+    
+   });
+   test('Select Radio2 marks it checked', async ({page}) => {
+      const radio2 = page.locator(`input[value="radio2"]`);
+      await radio2.check();
+      await expect(radio2).toBeChecked();
+   })
+});
